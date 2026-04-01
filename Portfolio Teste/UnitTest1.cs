729d0511fe3f8136a429672e6427f05c5dcbb10b
ADRYAN OLIVEIRA <adryan.26100462ead@alunos.unisagrado.edu.br>
2026-03-31 22:24:32 -0300

    feat: adiciona testes Playwright

---
+++ Portfolio Teste/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+public class PortfolioTests : PageTest
+{
+    private const string URL = "https://adriandotnet.com";
+
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync(URL);
+    }
+
+    // ... todos os testes aqui dentro ...
+
+    [Test]
+    public async Task LinkGitHubExiste()
+    {
+        var link = Page.Locator("a[href*='github.com']").First;
+        await Expect(link).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task LinkLinkedInExiste()
+    {
+        var link = Page.Locator("a[href*='linkedin.com']").First;
+        await Expect(link).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task SecaoProjetosVisivel()
+    {
+        var secao = Page.Locator("#projeto");
+        await Expect(secao).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task ProjetoOrdemServicoListado()
+    {
+        var body = Page.Locator("body");
+        await Expect(body).ToContainTextAsync("PROJETO DE ORDEM E SERVIÇO");
+    }
+}
